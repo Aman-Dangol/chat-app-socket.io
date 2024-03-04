0de80ab21414b0d17b8ed69c59ce3e564dd589ca
Aman Dangol <amandangol423@gmail.com>
2024-03-04 06:03:40 +0545

    created a simple socket

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
 const http = require("http");
 const fs = require("fs");
-
+const path = require("path");
 const server = http.createServer((req, res) => {
   if (req.url == "/") {
     res.setHeader("content-type", "text/html");
     let data = fs.readFileSync("./index.html");
     res.write(data);
     res.end();
-  }
-  if (req.url == "/styles/styless.css") {
+  } else if (req.url == "/styles/styles.css") {
     res.setHeader("content-type", "text/CSS");
-    let data = fs.readFileSync("./styles/styless.css");
+    let data = fs.readFileSync("./styles/styles.css");
     res.write(data);
     res.end();
   }
 });
 
 server.listen(3000);
+
+
+
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+io.on('connection',(socket)=>{
+  console.log("user connected");
+})
